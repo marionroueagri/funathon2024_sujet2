877114c0eb1e99bf0da115a755290fd41e5867ab
marionroue <marion.roue@insee.fr>
2024-06-26 14:32:26 +0000

    la fin de ce qu'on a pu faire dans le temps imparti 2

--- funathon2024_sujet1/donnees_trafic_aerien.R
+++ funathon2024_sujet1/donnees_trafic_aerien.R
@@ -11,5 +11,5 @@ source("funathon2024_sujet1/fonctions_traitement_avion.R")
 AIR_TRAFFIC_DATA_URL <- "https://www.data.gouv.fr/fr/datasets/r/0c0a451e-983b-4f06-9627-b5ff1bccd2fc"
 air_traffic_df <- read.csv2(AIR_TRAFFIC_DATA_URL)
 
-
+# Trafic aérien sur la ligne Paris Toulouse
 trafic_liaison_2019(air_traffic_df, "Paris", "Toulouse")
